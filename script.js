e3f03eadb013567e6ccf7fed9f83c27063a8d249
hargunrana <hargun.rana1@gmail.com>
2022-07-16 11:35:38 +0530

    🎇filter added

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// const { v1: uuidv1 } = require("uuid");
+const uid = new ShortUniqueId();
 const addBtn = document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
 const mainCont = document.querySelector(".main-cont");
 let colors = ["lightblue", "lightgreen", "lightpink", "black"];
 let modalPriorityColor = colors[1];
 let textAreaCont = document.querySelector(".textarea-cont");
+let toolboxColorCont = document.querySelectorAll(".color");
+
+let ticketsArr = [];
 
 let isModalPresent = false;
 addBtn.addEventListener("click", () => {
@@ -34,19 +39,67 @@ modalCont.addEventListener("keydown", (e) => {
         createTicket(modalPriorityColor, textAreaCont.value);
         modalCont.style.display = "none";
         isModalPresent = false;
+        textAreaCont.value = "";
     }
 });
 
-function createTicket(ticketColor, data) {
+function createTicket(ticketColor, data, ticketId) {
+    let id = ticketId || uid();
     let ticketCont = document.createElement("div");
 
     ticketCont.setAttribute("class", "ticket-cont");
     ticketCont.innerHTML = `
         <div class="ticket-color ${ticketColor}"></div>
-        <div class="ticket-id"></div>
+        <div class="ticket-id">#${id}</div>
         <div class="task-area"> ${data} </div>
 
     `;
 
     mainCont.appendChild(ticketCont);
+
+    if (!ticketId) {
+        ticketsArr.push({ ticketColor, data, ticketId: id });
+        localStorage.setItem("tickets", JSON.stringify(ticketsArr));
+    }
+}
+
+if (localStorage.getItem("tickets")) {
+    ticketsArr = JSON.parse(localStorage.getItem("tickets"));
+    ticketsArr.forEach((ticketObj) => {
+        createTicket(ticketObj.ticketColor, ticketObj.data, ticketObj.ticketId);
+    });
+}
+
+for (let i = 0; i < toolboxColorCont.length; i++) {
+    toolboxColorCont[i].addEventListener("click", (e) => {
+        let currColor = toolboxColorCont[i].classList[0];
+
+        let filterTickets = ticketsArr.filter((ticketObj) => {
+            return currColor == ticketObj.ticketColor;
+        });
+
+        let allTickets = document.querySelectorAll(".ticket-cont");
+
+        allTickets.forEach((e) => {
+            e.remove();
+        });
+
+        filterTickets.forEach((e) => {
+            createTicket(e.ticketColor, e.data, e.ticketId);
+        });
+    });
+
+    toolboxColorCont[i].addEventListener("dblclick", (e) => {
+        let allTickets = document.querySelectorAll(".ticket-cont");
+        allTickets.forEach((e) => {
+            e.remove();
+        });
+        ticketsArr.forEach((ticketObj) => {
+            createTicket(
+                ticketObj.ticketColor,
+                ticketObj.data,
+                ticketObj.ticketId
+            );
+        });
+    });
 }
